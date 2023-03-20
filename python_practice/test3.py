87d3efeb54a32e7da12114d56d48f47bcb88c7b2
Junyong <junong3737@gmail.com>
2023-03-20 14:38:25 +0900

    parameter test done

--- python_practice/test3.py
+++ python_practice/test3.py
@@ -1,9 +1,15 @@
-def test(*value, end="\n"): # type(value) -> tuple
-    print(value)
-    print(type(value))
-    print(end)
+def test(*value, end="\n", add=""): # type(value) -> tuple
+    print(f'value: {value}')
+    print(f'type(value): {type(value)}')
+
+    value = list(value)
+    print(f'type(list(value)): {type(value)}', f'value: {value}', sep="\n")
+    value.append(add)
+
+    print(f'end: {end}')
+    print("add: {}".format(add))
 
     for i in value:
         print(i, end=end)
 
-test('a', 'b', 'c', end="...")
+test('a', 'b', 'c', end="...", add="text")
