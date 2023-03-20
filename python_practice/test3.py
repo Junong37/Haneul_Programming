d251af5497dc702e442ad3673fbd85f5af3eb9cf
Junyong <junong3737@gmail.com>
2023-03-20 14:28:20 +0900

    parameter test

--- python_practice/test3.py
+++ python_practice/test3.py
@@ -1,1 +1,9 @@
-print("hello world")
+def test(*value, end="\n"):
+    print(value)
+    print(type(value))
+    print(end)
+
+    for i in value:
+        print(i, end=end)
+
+test('a', 'b', 'c', end="...")
