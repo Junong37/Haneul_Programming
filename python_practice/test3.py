63d0d1ce3b302fd15dff070d02eac33224d8c41f
Junyong <junong3737@gmail.com>
2023-03-23 21:34:13 +0900

    2023-03-23 clean

--- python_practice/test3.py
+++ python_practice/test3.py
@@ -1,15 +1,1 @@
-def test(*value, end="\n", add=""): # type(value) -> tuple
-    print(f'value: {value}')
-    print(f'type(value): {type(value)}')
-
-    value = list(value)
-    print(f'type(list(value)): {type(value)}', f'value: {value}', sep="\n")
-    value.append(add)
-
-    print(f'end: {end}')
-    print("add: {}".format(add))
-
-    for i in value:
-        print(i, end=end)
-
-test('a', 'b', 'c', end="...", add="text")
+print("hello" * 3)
