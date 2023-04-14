8ed3ff3366c508ee6ead58fa94b17793f8446323
Junyong <junong3737@gmail.com>
2023-04-14 10:24:46 +0900

    2023-04-14

--- python_practice/test4.py
+++ python_practice/test4.py
@@ -1,12 +1,16 @@
+#369
+import time
+
 i = 0
+
 while True:
+    time.sleep(0.1)
     i += 1
-    a = int(input())
-    
-    if a >= 10:
+    i = str(i)
+
+    if "3" in i or "6" in i or "9" in i:
+        print("짝")
+    else:
         print(i)
-        break
-    elif a == 5:
-        print("hi")
-        continue
-    print(i)
+        
+    i = int(i)
